d7d016b83fde3ef5cac811991ce7d529b9881423
g_rocket <g.rocket1@gmail.com>
2015-04-22 23:42:03 -0400

    fix bug in PointSeeder was passing width instead of height, which led to overflow

--- src/net/clonecomputers/lab/starburst/seed/PointSeeder.java
+++ src/net/clonecomputers/lab/starburst/seed/PointSeeder.java
@@ -1,12 +1,12 @@
 package net.clonecomputers.lab.starburst.seed;
 
 import static java.lang.Math.*;
+import static net.clonecomputers.lab.starburst.util.StaticUtils.*;
 
 import java.awt.*;
 
 import net.clonecomputers.lab.starburst.*;
 import net.clonecomputers.lab.starburst.properties.*;
-import static net.clonecomputers.lab.starburst.util.StaticUtils.*;
 
 public class PointSeeder extends Seeder {
 	public PointSeeder(Starburst s) {
@@ -15,7 +15,7 @@ public class PointSeeder extends Seeder {
 	
 	public void seedImage(PropertyTreeNode properties) {
 		for(int i = 0; i < properties.getAsInt("howMany"); i++) {
-			Point p = randomRadialBiasedPoint(properties.getAsDouble("distribution"), s.getImageWidth(), s.getImageWidth());
+			Point p = randomRadialBiasedPoint(properties.getAsDouble("distribution"), s.getImageWidth(), s.getImageHeight());
 			if(s.current[p.x][p.y]) continue;
 			s.operations.addPoint(p.x, p.y);
 			s.current[p.x][p.y] = true;
@@ -24,13 +24,13 @@ public class PointSeeder extends Seeder {
 	}
 
 	private Point randomRadialBiasedPoint(double bias, int width, int height) {
-		Point p = new Point(-1, -1); // intentionally invalid
-		while(p.x < 0 || p.y < 0 || p.x >= width || p.y >= height) { // keep trying until valid
+		Point p;
+		do {
 			double angle = s.rand.nextDouble() * 2*PI;
 			double radiasBais = bias==0? 0: pow(s.rand.nextDouble(), log(bias)/log(.5));
 			double radius = radiasBais * sqrt(width*width + height*height)/2; // maximum possible, may not be valid
 			p = new Point((int)(radius*cos(angle)) + width/2, (int)(radius*sin(angle)) + height/2);
-		}
+		} while(p.x < 0 || p.y < 0 || p.x >= width || p.y >= height); // keep trying until valid
 		return p;
 	}
 
