3af56d2d555480858db7acaf512108ce43586f54
g_rocket <g.rocket1@gmail.com>
2015-04-28 21:54:37 -0400

    allow AbstractNumberProperty (and subclasses) to be set using any Number
    (uses reflection, so it's a bit of a hack)

--- src/net/clonecomputers/lab/starburst/properties/AbstractNumberProperty.java
+++ src/net/clonecomputers/lab/starburst/properties/AbstractNumberProperty.java
@@ -2,6 +2,7 @@ package net.clonecomputers.lab.starburst.properties;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.lang.reflect.*;
 import java.util.*;
 
 import javax.swing.*;
@@ -19,7 +20,9 @@ public abstract class AbstractNumberProperty<T extends Number> extends AbstractP
 	protected double max;
 	protected JTextField textBox;
 	protected JSlider slider;
-
+	@SuppressWarnings("unchecked")
+	private boolean isDiscrete = isDiscrete((Class<? extends Number>) type.getRawType());
+	
 	@SuppressWarnings("unchecked")
 	public AbstractNumberProperty(String name, String category, JsonObject data, Random r) {
 		super(name, category, data);
@@ -40,19 +43,61 @@ public abstract class AbstractNumberProperty<T extends Number> extends AbstractP
 			min = Double.NEGATIVE_INFINITY;
 			max = Double.POSITIVE_INFINITY;
 		}
-		if(isDiscrete((Class<? extends Number>) type.getRawType())) {
+		if(isDiscrete) {
 			if((!Double.isInfinite(max) && max != Math.round(max)) ||
 			   (!Double.isInfinite(max) && max != Math.round(max))) {
 				throw new IllegalArgumentException(String.format("Json passed to %s is invalid: max and min are not both integers: [%f,%f]", getClass().getCanonicalName(), max, min));
 			}
 		}
-		if(!Double.isInfinite(max) && isDiscrete((Class<? extends Number>) type.getRawType())) max = Math.round(max);
-		if(!Double.isInfinite(min) && isDiscrete((Class<? extends Number>) type.getRawType())) min = Math.round(min);
 		this.r = Randomizer.createRandomizer((Class<T>)type.getRawType(), r, min, max, data.get("random"));
 	}
 	
+	@Override
+	public boolean isValid(Object newValue) {
+		if(!(newValue instanceof Number)) return false;
+		return isValidNumber((Number)newValue);
+	}
+	
+	/**
+	 * this returns if newValue is in range (with a double) by default,
+	 * but should be overriden if more advanced validity checks are needed
+	 * @param newValue has already been checked to be of the right type
+	 * @return whether newValue is a valid value
+	 */
+	protected boolean isValidNumber(Number newValue) {
+		if(isDiscrete(newValue.getClass()) != isDiscrete) return false;
+		return min <= newValue.doubleValue() && newValue.doubleValue() <= max;
+	}
+
 	private static boolean isDiscrete(Class<? extends Number> c) {
 		return !(c.isInstance(Double.class) || c.isInstance(Float.class));
+	}
+	
+	@Override
+	public boolean isValidValue(T newValue) {
+		return (min <= newValue.doubleValue()) && (newValue.doubleValue() <= max);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public void setValue(Object newValue) {
+		if(!isValid(newValue)) {
+			throw new IllegalArgumentException(newValue+" is not a valid value");
+		}
+		try {
+			String typeName = type.unwrap().getRawType().getName();
+			value = (T) Number.class.getMethod(typeName+"Value").invoke(newValue);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalArgumentException e) {
+			throw new RuntimeException(e);
+		} catch (InvocationTargetException e) {
+			throw new RuntimeException(e);
+		} catch (NoSuchMethodException e) {
+			throw new RuntimeException(e);
+		} catch (SecurityException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public AbstractNumberProperty(String name, String category, double min, double max, Randomizer<T> r) {
@@ -68,11 +113,6 @@ public abstract class AbstractNumberProperty<T extends Number> extends AbstractP
 		if(r.canRandomize()) {
 			value = r.randomize();
 		}
-	}
-	
-	@Override
-	public boolean isValidValue(T newValue) {
-		return (min <= newValue.doubleValue()) && (newValue.doubleValue() <= max);
 	}
 
 	@Override
